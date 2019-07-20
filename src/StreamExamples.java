f80ca9b7a8c09c2b5255bc50ced99c2b90f9a619
Atul Kumar Verma <atul05kumar@users.noreply.github.com>
2019-07-21 00:28:13 +0530

    Added more examples

--- src/StreamExamples.java
+++ src/StreamExamples.java
@@ -7,7 +7,22 @@ public class StreamExamples {
                 .range(1, 10)
                 .forEach(System.out::print);
 
+        System.out.println();
 
+        /*Invoking a function for each element of the stream using lambda notation
+        * Use of skip intermediate operation.
+        * */
+        IntStream.range(1, 10)
+                .skip(5)
+                .forEach(x -> System.out.print(x));
+
+        System.out.println();
+
+        /*Use of sum terminal operation*/
+        int sumOfOneToNine = IntStream
+                .range(1,10)
+                .sum();
+        System.out.println(sumOfOneToNine);
 
     }
 }
